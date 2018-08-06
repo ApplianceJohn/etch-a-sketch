192d64a8b734048c8f41e754cf7e5ce3058fe47b
ApplianceJohn <thednbgremlin@gmail.com>
2018-08-06 13:03:49 -0400

    Add logic for drawing lines and changing grid dimensions

--- logic.js
+++ logic.js
@@ -1,26 +1,44 @@
 const grid = document.getElementById("grid");
 
-var gridSize = 8;
+var gridSize = 64;
 
 function draw(e) {
-    this.style.backgroundColor("black");
+    this.style.backgroundColor = "black";
 }
 
-//function generator(size) =>
-while (grid.firstChild) {
-    grid.removeChild(grid.firstChild);
-}
-
-for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-        var d = document.createElement("div");
-        d.className = "square";
-        d.addEventListener('onmouseover', draw);
-        d.style.width = `calc(100% / ${gridSize})`;
-        d.style.height = `calc(100% / ${gridSize})`;
-        grid.appendChild(d);
+function redraw(e) {
+    var input = parseInt(document.getElementById("insert").value);
+    
+    if (isNaN(input)) {
+        alert("Input must be a valid number!");
+        return;
     }
-    var br = document.createElement('br');
-    grid.appendChild(br);
+
+    gridSize = input;
+    generator();
 }
-//=> end generator
+
+function generator() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            var d = document.createElement("div");
+            d.addEventListener("mouseover", draw);
+            d.className = "square";
+            d.style.width = `calc(100% / ${gridSize})`;
+            d.style.height = `calc(100% / ${gridSize})`;
+            grid.appendChild(d);
+        }
+        var br = document.createElement('br');
+        grid.appendChild(br);
+    }
+
+}
+
+var button = document.getElementById("butt");
+button.addEventListener("click", redraw);
+
+generator();
